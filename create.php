d485a1f7b0ce01cc2e6448bb55e4d1451506900f
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-03-18 22:42:40 -0300

    feat: implementa formulário de cadastro de estudante

---
+++ create.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>Cadastrar Novo Estutante</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
+    <!-- Bootstrap Icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"/>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic"
+          rel="stylesheet" type="text/css"/>
+    <!-- SimpleLightbox plugin CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet"/>
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet"/>
+</head>
+<body id="page-top">
+<!-- Contact-->
+<section class="page-section" id="contact">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
+            <div class="col-lg-6">
+                <form id="contactForm" action="index.php" method="post">
+                    <!-- Nome -->
+                    <div class="form-floating mb-3">
+                        <input class="form-control" name="name" type="text" placeholder="Nome Completo" required/>
+                        <label for="name">Nome Completo</label>
+                    </div>
+
+                    <!-- Email -->
+                    <div class="form-floating mb-3">
+                        <input class="form-control" name="mail" type="email" placeholder="Email" required/>
+                        <label for="mail">E-mail</label>
+                    </div>
+
+                    <!-- Matrícula -->
+                    <div class="form-floating mb-3">
+                        <input class="form-control" name="registration" type="text" placeholder="Matrícula" required/>
+                        <label for="registration">Matrícula</label>
+                    </div>
+
+                    <!-- Matrícula -->
+                    <div class="form-floating mb-3">
+                        <select class="form-control" name="status" required>
+                            <option value="" disabled selected>Selecione uma das opções abaixo</option>
+                            <option value="ativo">Ativo</option>
+                            <option value="inativo">Inativo</option>
+                        </select>
+                        <label for="registration">Matrícula</label>
+                    </div>
+
+                    <!-- Data de Nascimento -->
+                    <div class="form-floating mb-3">
+                        <input class="form-control" name="date_of_birth" type="date" placeholder="Data de Nascimento"
+                               required/>
+                        <label for="date_of_birth">Data de Nascimento</label>
+                    </div>
+
+
+                    <button class="btn btn-primary" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- Footer-->
+<footer class="bg-light py-5">
+    <div class="container px-4 px-lg-5">
+        <div class="small text-center text-muted"><?= date("Y") . " - iG7 Tecnologia" ?></div>
+    </div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- SimpleLightbox plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<!-- * *                               SB Forms JS                               * *-->
+<!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -4,13 +4,72 @@ require __DIR__ . "/Connection.php";
 
 try {
 
-    echo "Teste de conexão com o banco de dados.</br>";
-
     $connection = Connection::getInstance();
 
-    var_dump($connection);
+    $name = $_POST["name"];
+    $mail = $_POST["mail"];
+    $registration = $_POST["registration"];
+    $status = $_POST["status"];
+    $date_of_birth = $_POST["date_of_birth"];
 
-    echo "Conexão com o banco de dados bem sucedida!";
+    var_dump($_POST);
+
+    $name = trim(strip_tags($name));
+
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        echo "Atenção: Não é um e-mail válido!";
+        die;
+    }
+
+    $queryEmailExists = "SELECT * FROM students WHERE mail = :mail";
+    $statementEmailExists = $connection->prepare($queryEmailExists);
+    $statementEmailExists->bindParam(":mail", $mail);
+    $statementEmailExists->execute();
+    $resultEmailExists = $statementEmailExists->fetchAll();
+
+    if (!empty($resultEmailExists)) {
+        echo "Atenção: Esse e-mail já foi cadastrado!";
+        die;
+    }
+
+    $queryRegistrationExists = "SELECT * FROM students WHERE registration = :registration";
+    $statementRegistrationExists = $connection->prepare($queryRegistrationExists);
+    $statementRegistrationExists->bindParam(":registration", $registration);
+    $statementRegistrationExists->execute();
+    $resultRegistrationExists = $statementRegistrationExists->fetchAll();
+
+    if (!empty($resultRegistrationExists)) {
+        echo "Atenção: Essa matricula já foi cadastrada!";
+        die;
+    }
+
+    $permittedStatus = [
+        "ativo",
+        "inativo"
+    ];
+
+    if (!in_array($status, $permittedStatus, true)) {
+        echo "Atenção: Status inválido!";
+        die;
+    }
+
+    $newStudent = [
+        "name" => $name,
+        "mail" => $mail,
+        "registration" => $registration,
+        "status" => $status,
+        "date_of_birth" => $date_of_birth
+    ];
+
+    $queryInsertStudent = "INSERT INTO students (name, mail, registration, status, date_of_birth)
+                            VALUES (:name, :mail, :registration, :status, :date_of_birth)";
+
+    $statementInsertStudent = $connection->prepare($queryInsertStudent);
+    $student = $statementInsertStudent->execute($newStudent);
+
+    if ($student) {
+        echo "Sucesso: novo estudante cadastrado com sucesso: " . $connection->lastInsertId();
+    }
 
 } catch (\RuntimeException $runtimeException) {
     echo "Erro: " . $runtimeException->getMessage();
